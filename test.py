329b69257788901657be7810a82c02d397f1b54f
Oscar Ramos, Jr. <potatobucket223@gmail.com>
2026-02-15 13:35:26 -0600

    Create test.py
    Created the repository and added a small Python script to test whether or not I actually cloned it.

---
+++ test.py
@@ -0,0 +1,8 @@
+from time import sleep
+
+if __name__ == ""__main__":
+    print("Successful test!")
+    sleep(1)
+    print("You successfully cloned a GitHub repository!")
+    sleep(1)
+    print("Congratulations, ya jerk!")

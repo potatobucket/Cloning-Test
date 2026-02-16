c55a582415fbdd0ee838a02bd360e69f50019f56
Oscar Ramos, Jr. <potatobucket223@gmail.com>
2026-02-15 18:09:19 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 from time import sleep
 
-if __name__ == ""__main__":
-    print("Successful test!")
-    sleep(1)
-    print("You successfully cloned a GitHub repository!")
-    sleep(1)
-    print("Congratulations, ya jerk!")
+print("Successful test!")
+sleep(1)
+print("You successfully cloned a GitHub repository!")
+sleep(1)
+print("Congratulations, ya jerk!")
